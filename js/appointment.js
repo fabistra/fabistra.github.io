35c0e6a0917dfbfd1f1f71ac6ba66ba4b6355789
Rajat Bhandari <32290341+fabistra@users.noreply.github.com>
2021-03-10 17:58:49 +0530

    contact page setup

---
+++ js/appointment.js
@@ -0,0 +1,31 @@
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbxNpy8KJEqp_WyHp2OJJ9bCINWiOXQe4EIM1gq1nr96vu3E5OQ/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
+
+$.fn.serializeObject = function()
+{
+   var o = {};
+   var a = this.serializeArray();
+   $.each(a, function() {
+       if (o[this.name]) {
+           if (!o[this.name].push) {
+               o[this.name] = [o[this.name]];
+           }
+           o[this.name].push(this.value || '');
+       } else {
+           o[this.name] = this.value || '';
+       }
+   });
+   return o;
+};
